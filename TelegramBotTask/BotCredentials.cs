80d0f89ea363f000d5fe33aa7a292b039f3341ac
Sergey <ssm1988@inbox.ru>
2021-05-13 13:40:56 +1000

    token

--- TelegramBotTask/BotCredentials.cs
+++ TelegramBotTask/BotCredentials.cs
@@ -8,6 +8,6 @@ namespace TelegramBotTask
 {
     public static class BotCredentials
     {
-        public static readonly string BotToken = "1704549394:AAFRtkcYa2PhfYtZa64RFlEgqh4_ZC2-VMQ";
+        public static readonly string BotToken = "token";
     }
 }
